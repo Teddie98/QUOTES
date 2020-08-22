b9228047c1b2df4be0fdc8235f212497079f35e4
Teddie <teddymachoka98@yahoo.com>
2020-08-22 21:56:56 +0300

    new changes in the highlight.directive.ts

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     FormComponent,
-    DetailsComponent
+    DetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
