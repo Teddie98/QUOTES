580d176064f933b00d22b300454662ed230f8abf
Teddie <teddymachoka98@yahoo.com>
2020-08-22 21:31:47 +0300

    add changes in the form component.html

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
